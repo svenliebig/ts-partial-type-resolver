d68d1432e1e2fb22585f1d8835ad1d22d6d6ee2e
Sven Liebig <sven.liebig@nortal.com>
2022-08-25 00:16:00 -0700

    add the possibility to filter logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,20 @@
 let mode: "on" | "off" = 'off'
-
+let filter: null | string = null
 export class L {
 	static off() {
 		mode = "off"
-	}	
+	}
+
 	static on() {
 		mode = "on"
 	}
 
+	static filter(value: string | null) {
+		filter = value
+	}
+
 	static d(...args: Array<any>) {
-		if (mode === "on") {
+		if (mode === "on" && (filter === null || args.join("").toLocaleLowerCase().includes(filter.toLocaleLowerCase()))) {
 			console.log(...args)
 		}
 	}
