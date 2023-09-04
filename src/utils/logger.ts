9c134a2b5b1b5d860b232fc64273c5ecce24587b
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2023-09-04 17:21:48 +0200

    feat: improve logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,8 @@
 let mode: "on" | "off" = "off"
 let filter: null | string = null
+
+const layers: Array<string> = []
+
 export const L = {
 	off() {
 		mode = "off"
@@ -18,16 +21,69 @@ export const L = {
 	},
 
 	d: console.log.bind(console),
+
+	/** a successful check */
+	s: console.log.bind(console, "✅"),
+
+	/** a failed check */
+	f: console.log.bind(console, "❌"),
+
+	enter(identifier: string) {
+		layers.push(identifier)
+	},
+
+	exit() {
+		layers.pop()
+	},
 }
 
 Object.defineProperty(L, "d", {
 	get: () => {
 		if (mode === "on") {
 			if (filter) {
-				return (...args: Array<any>) => console.log.apply(console, args)
+				return (...args: Array<any>) => console.log.apply(console, [...args, printLayers(layers)])
 			}
-			return console.log.bind(console)
+			return (...args: Array<any>) => console.log.apply(console, [...args, printLayers(layers)])
 		}
 		return function () {}
 	},
 })
+
+Object.defineProperty(L, "f", {
+	get: () => {
+		if (mode === "on") {
+			if (filter) {
+				return (...args: Array<any>) => console.log.apply(console, ["❌", ...args, printLayers(layers)])
+			}
+
+			return (...args: Array<any>) => console.log.apply(console, ["❌", ...args, printLayers(layers)])
+		}
+		return function () {}
+	},
+})
+
+Object.defineProperty(L, "s", {
+	get: () => {
+		if (mode === "on") {
+			if (filter) {
+				return (...args: Array<any>) => console.log.apply(console, ["✅", ...args, printLayers(layers)])
+			}
+
+			return (...args: Array<any>) => console.log.apply(console, ["✅", ...args, printLayers(layers)])
+		}
+		return function () {}
+	},
+})
+
+function printLayers(layers: Array<string>) {
+	if (layers.length === 0) {
+		return ""
+	}
+
+	const rev = [...layers].reverse()
+	return "\n" + rev.map(printLayer).join("\n")
+}
+
+function printLayer(layer: string) {
+	return `    from ${layer.substring(0, 220)}`
+}
