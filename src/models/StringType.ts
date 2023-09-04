5a0bb11af6f3949d66d4382361c7137b0354ca09
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2023-09-04 17:23:18 +0200

    chore: remove unused imports

--- src/models/StringType.ts
+++ src/models/StringType.ts
@@ -1,6 +1,4 @@
-import { StringTypeDeclaration } from "./StringTypeDeclaration"
 import { Type } from "./Type"
-import { TypeDeclaration } from "./TypeDeclaration"
 
 export class StringType extends Type {
 	constructor(public identifier: string) {
